e7bf0da673885b5e9d60e8e10de7620e96e6ea7e
leemw2 <leemw2@vcu.edu>
2025-02-21 19:36:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function generateJoke() {
     const res = await fetch('https://icanhazdadjoke.com', config);
     const data = await res.json();
   
-    jokeEl.innerHTML = data.joke;
+    jokeEl.textContent = data.joke;
   }
 
 jokeBtn.addEventListener('click', generateJoke);
